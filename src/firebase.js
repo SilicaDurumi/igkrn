8be837d08788bcedbc13535f3c86d727f3f7e4e3
Silica Durumi <Silica@Durumi.com>
2021-02-18 14:48:16 +0900

    2021-02-18-01

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD4-YyUDltjkaf3bJWB8R7EA9Xp2qo7jio",
+    authDomain: "igkrn-1e5ef.firebaseapp.com",
+    projectId: "igkrn-1e5ef",
+    storageBucket: "igkrn-1e5ef.appspot.com",
+    messagingSenderId: "447804498019",
+    appId: "1:447804498019:web:cfdd93bc9539bfd7401856"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
